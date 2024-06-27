f34cdd4acee248e08eddc619aeece9dd487397e3
Hwangyongjin <dswvgw6412@naver.com>
2024-06-27 16:45:07 +0900

    :memo:Modify: 버튼 컴포넌트 hover 및 inline-css 추가

--- NeighborChatFE/src/components/Common/Button.jsx
+++ NeighborChatFE/src/components/Common/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-const Button = ({ onClick, children }) => {
+const Button = ({ onClick, children, style }) => {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button} onClick={onClick} style={style}>
       {children}
     </button>
   );
