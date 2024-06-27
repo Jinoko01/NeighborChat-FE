cec533a3aa7090553f7e9c691ea4392bcd7c7c0b
ttatjwi <su_zero_@naver.com>
2024-06-27 18:14:32 +0900

    ✨  정보수정, 톱니바퀴 기능 추가

--- NeighborChatFE/src/pages/Mainpage.jsx
+++ NeighborChatFE/src/pages/Mainpage.jsx
@@ -1,10 +1,14 @@
+import Modal from "../components/Common/Modal.jsx";
 import MapComponent from "../components/Mainpage/MapComponent.jsx";
+import Bar from '../components/Common/Bar';
+
 
 const Mainpage = () => {
 
   return(
     <div>
       <MapComponent />
+      <Bar />
     </div>
   );
 };
--- NeighborChatFE/src/pages/ProfilePage.jsx
+++ NeighborChatFE/src/pages/ProfilePage.jsx
@@ -1,32 +1,76 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 import Modal from '../components/Common/Modal';
 import MapComponent from '../components/Mainpage/MapComponent';
 import Button from '../components/Common/Button';
 import ProfileImage from '../assets/Profile.png'
-import styles from '../components/Common/Profile.module.css';
+import styles from '../components/Common/Pages.module.css';
 
 
 const ProfilePage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const [nickname, setNickname] = useState('닉네임');
+  const [intro, setIntro] = useState('한 줄 소개(10자 이내)');
 
-  const closeModal = () => setIsModalOpen(false);
+  const toggleEditMode = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleNicknameChange = (e) => {
+    setNickname(e.target.value);
+  };
+
+  const handleIntroChange = (e) => {
+    setIntro(e.target.value);
+  };
 
   return (
     <div style={{ position: 'relative', height: '100vh' }}>
       <MapComponent />
-      {isModalOpen && (
-        <Modal>
-          <h1>프로필</h1>
-          <div className={styles.picture}>
+      <Modal>
+        <div>
+          <h1 className={styles.logo}>프로필</h1>
+
+          <div className={styles.inner}>
             <div className={styles.circle}>
               <img className={styles.profile} src={ProfileImage} alt="프로필_사진" />
             </div>
+            <div className={styles.contents}>
+              <div className={styles.details}>
+                {isEditing ? (
+                  <>
+                  <input
+                  type="text"
+                  className={styles.input}
+                  value={nickname}
+                  onChange={handleNicknameChange} />
+                  <input
+                  type="text"
+                  className={styles.input}
+                  maxlength='10'
+                  value={intro}
+                  onChange={handleIntroChange} />
+                  </>) :
+                  (
+                  <>
+                    <label className={styles.label}>{nickname}</label>
+                    <label className={styles.label}>{intro}</label>
+                  </>
+                  )}
+              </div>
+              <Button onClick={toggleEditMode}>
+              {isEditing ? '저장' : '정보수정'}
+              </Button>
+            </div>
           </div>
-          <Button onClick={closeModal}>
-            정보수정
-          </Button>
-        </Modal>
-      )}
+
+          <div className={styles.footer}>
+                <Link to="/register" className={styles.link}>회원가입</Link>
+                <Link to="/forgotPassword" className={styles.link}>비밀번호 찾기</Link>
+          </div>
+          
+        </div>
+      </Modal>
     </div>
   );
 };
