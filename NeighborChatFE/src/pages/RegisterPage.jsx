4ddfa3e7b691082bb529b492fc332df3a54176be
Hwangyongjin <dswvgw6412@naver.com>
2024-07-02 22:46:40 +0900

    :memo:Modify: UI 보완

--- NeighborChatFE/src/pages/RegisterPage.jsx
+++ NeighborChatFE/src/pages/RegisterPage.jsx
@@ -7,6 +7,22 @@ import {NavermapsProvider} from "react-naver-maps";
 import {Link} from "react-router-dom";
 
 const RegisterPage = () => {
+  const [register, setRegister] = useState({
+    accountLoginId: '',
+    accountLoginPw: '',
+    accountLoginPwCheck: '',
+  });
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setRegister({
+      ...register,
+      [name]: value,
+    });
+
+    console.log(register);
+  };
 
   return (
     <div style={{ position: 'relative', height: '100vh' }}>
@@ -16,23 +32,29 @@ const RegisterPage = () => {
       <Modal>
         <div className={styles.contentContainer}>
           <h1 className={styles.logo}>회원가입</h1>
-
           <input
             type="text"
             className={styles.input}
             placeholder="아이디"
+            name="accountLoginId"
+            value={register.accountLoginId}
+            onChange={handleChange}
           />
-
           <input
             type="password"
             className={styles.input}
             placeholder="비밀번호"
+            name="accountLoginPw"
+            value={register.accountLoginPw}
+            onChange={handleChange}
           />
-
           <input
             type="password"
             className={styles.input}
             placeholder="비밀번호 확인"
+            name="accountLoginPwCheck"
+            value={register.accountLoginPwCheck}
+            onChange={handleChange}
           />
           <Button>
             회원가입
